f0f10006cad813df0989827644815c53d2b06304
Giorgio02K <jordichorrog96@gmail.com>
2023-10-23 18:44:58 +0200

    login completado

--- app/src/main/java/com/tema4actividad2/jordichorro/MainActivity.kt
+++ app/src/main/java/com/tema4actividad2/jordichorro/MainActivity.kt
@@ -3,6 +3,9 @@ package com.tema4actividad2.jordichorro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity {
-
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
