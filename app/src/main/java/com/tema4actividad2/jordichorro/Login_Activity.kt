d23bfeaf33d44c3466fb876bcc6c600a3032e540
Giorgio02K <jordichorrog96@gmail.com>
2023-10-23 13:01:21 +0200

    Initial commit

---
+++ app/src/main/java/com/tema4actividad2/jordichorro/Login_Activity.kt
@@ -0,0 +1,11 @@
+package com.tema4actividad2.jordichorro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Login_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
---
+++ app/src/main/java/com/tema4actividad2/jordichorro/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.tema4actividad2.jordichorro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity {
+
+}
